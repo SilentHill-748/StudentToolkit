65850ceb67511c61c6a83c028330c7a2357a3f2d
SilentHill <nikich213@gmail.com>
2023-09-19 02:15:06 +0500

    Add project.

---
+++ src/StudentToolkit/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace StudentToolkit;
+
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        
+    }
+}
