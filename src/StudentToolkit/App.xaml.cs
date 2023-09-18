8f3f78514db660451a62e7b2542363c2c6ab6e00
SilentHill <nikich213@gmail.com>
2023-09-19 03:29:40 +0500

    Add DI container 'SimpleInjector'.

--- src/StudentToolkit/App.xaml.cs
+++ src/StudentToolkit/App.xaml.cs
@@ -2,10 +2,25 @@
 
 public partial class App : Application
 {
+    private readonly Container _container = new();
+
     protected override void OnStartup(StartupEventArgs e)
     {
-        MainWindow = new MainWindow(new MainViewModel());
+        AddServices();
 
+        MainWindow = _container.GetInstance<MainWindow>();
         MainWindow.Show();
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        _container.Dispose();
+
+        base.OnExit(e);
+    }
+
+    private void AddServices()
+    {
+        _container.RegisterServices();
     }
 }
---
+++ src/StudentToolkit/Configuration/DI/RegisterServicesExtention.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentToolkit.Configuration.DI;
+
+public static class RegisterServicesExtention
+{
+    public static void RegisterServices(this Container container)
+    {
+        container
+            .RegisterViewModels()
+            .RegisterViews();
+
+        container.Verify();
+    }
+}
