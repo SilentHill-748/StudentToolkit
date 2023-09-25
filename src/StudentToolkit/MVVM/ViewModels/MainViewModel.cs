8edd5b7f16865704116a0b545bc5d427ec3cc60d
SilentHill <nikich213@gmail.com>
2023-09-25 11:04:38 +0500

    Add window content navigation. Add 'CallerMemberName' for raise property changed event in 'ViewModel.cs'.

--- src/StudentToolkit/MVVM/ViewModels/MainViewModel.cs
+++ src/StudentToolkit/MVVM/ViewModels/MainViewModel.cs
@@ -1,6 +1,33 @@
-﻿namespace StudentToolkit.MVVM.ViewModels;
+﻿using CommunityToolkit.Mvvm.Messaging;
+
+using StudentToolkit.MVVM.Models.Navigation.Messages;
+
+namespace StudentToolkit.MVVM.ViewModels;
 
 public class MainViewModel : ViewModel
 {
+    private ViewModel _currentViewMdoel;
 
+    public MainViewModel(ViewModel defaultViewModel)
+    {
+        _currentViewMdoel = defaultViewModel;
+
+        WeakReferenceMessenger.Default.Register<WindowContentNavigationMessage>(this, OnNavigated);
+    }
+
+    public ViewModel CurrentViewModel
+    {
+        get => _currentViewMdoel;
+        set => Set(ref _currentViewMdoel, value);
+    }
+
+    private void OnNavigated(object recipient, WindowContentNavigationMessage message)
+    {
+        if (message.Value is NavigationModel model)
+        {
+            CurrentViewModel = model.DestinationViewModel;
+
+            WindowTitle = model.DestinationViewModel.WindowTitle;
+        }
+    }
 }
