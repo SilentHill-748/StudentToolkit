fccdae489ee29256fbc0eec934bde74d7e6a0246
SilentHill <nikich213@gmail.com>
2023-09-25 10:49:23 +0500

    Add 'NavigationModel.cs' and 'WindowContentNavigationMessage.cs'.

---
+++ src/StudentToolkit/MVVM/Models/Navigation/Messages/WindowContentNavigationMessage.cs
@@ -0,0 +1,9 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+
+namespace StudentToolkit.MVVM.Models.Navigation.Messages;
+
+internal sealed class WindowContentNavigationMessage : ValueChangedMessage<NavigationModel>
+{
+    public WindowContentNavigationMessage(NavigationModel value) 
+        : base(value) { }
+}
---
+++ src/StudentToolkit/MVVM/Models/Navigation/NavigationModel.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentToolkit.MVVM.Models.Navigation;
+
+internal sealed class NavigationModel
+{
+    public NavigationModel(ViewModel viewModel)
+    {
+        DestinationViewModel = viewModel;
+    }
+
+    public ViewModel DestinationViewModel { get; set; }
+}
