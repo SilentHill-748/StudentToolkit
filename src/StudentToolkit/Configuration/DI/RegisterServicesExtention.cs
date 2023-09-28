388f41fed5fde35cb88158711e756ff0f7bdbd46
SilentHill <nikich213@gmail.com>
2023-09-28 14:54:21 +0500

    Correct 'DataTemplateService', add extention methods for 'Assembly', cleanup.

--- src/StudentToolkit/Configuration/DI/RegisterServicesExtention.cs
+++ src/StudentToolkit/Configuration/DI/RegisterServicesExtention.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 
 namespace StudentToolkit.Configuration.DI;
+
 public static class RegisterServicesExtention
 {
     public static Container RegisterServices(this Container container)
     {
+        var assembly = typeof(RegisterServicesExtention).Assembly;
+
         container.RegisterSingleton<NavigationService>();
-        container.RegisterSingleton<DataTemplateService>();
+        container.RegisterSingleton(() => new DataTemplateService(assembly));
 
         container.RegisterSingleton<Func<Type, object>>(() => container.GetInstance);
 
