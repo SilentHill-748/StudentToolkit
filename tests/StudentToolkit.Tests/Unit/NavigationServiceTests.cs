abc8940fefd7359b29bbc8c108b867949f5195b1
SilentHill <nikich213@gmail.com>
2023-09-30 13:24:30 +0500

    Move view model collection and func delegate to test cases.

--- tests/StudentToolkit.Tests/Unit/NavigationServiceTests.cs
+++ tests/StudentToolkit.Tests/Unit/NavigationServiceTests.cs
@@ -2,28 +2,16 @@
 
 public class NavigationServiceTests
 {
-    private readonly Dictionary<Type, ViewModel> _viewModels;
-    private readonly Func<Type, object> _viewModelResolver;
-
-    public NavigationServiceTests()
-    {
-        _viewModels = new Dictionary<Type, ViewModel>();
-        _viewModelResolver = (type) => _viewModels[type];
-
-        InitialViewModels();
-    }
-
     [Fact]
     public void Should_success_navigation()
     {
-        // Arrange
-        NavigationService navigationService = new(_viewModelResolver);
-        NavigationViewModel navigationViewModel = (NavigationViewModel)_viewModelResolver(typeof(NavigationViewModel));
+        Dictionary<Type, ViewModel> viewModels = CreateViewModelCollection();
+        Func<Type, ViewModel> viewModelResolver = (type) => viewModels[type];
+        NavigationService navigationService = new(viewModelResolver);
+        NavigationViewModel navigationViewModel = (NavigationViewModel)viewModelResolver(typeof(NavigationViewModel));
 
-        // Act
         navigationService.NavigateTo<StubNavigatingViewModel, WindowContentNavigationMessage>(new WindowNavigationQuery());
 
-        //Accept
         Assert.NotNull(navigationViewModel.CurrentViewModel);
         Assert.IsNotType<MainViewModel>(navigationViewModel.CurrentViewModel);
         Assert.IsType<StubNavigatingViewModel>(navigationViewModel.CurrentViewModel);
@@ -32,38 +20,41 @@ public class NavigationServiceTests
     [Fact]
     public void Should_throw_navigation_denied_exception_by_window_navigate()
     {
-        // Arrange
-        NavigationService navigationService = new(_viewModelResolver);
-        NavigationViewModel navigationViewModel = (NavigationViewModel)_viewModelResolver(typeof(NavigationViewModel));
+        Dictionary<Type, ViewModel> viewModels = CreateViewModelCollection();
+        Func<Type, ViewModel> viewModelResolver = (type) => viewModels[type];
+        NavigationService navigationService = new(viewModelResolver);
+        NavigationViewModel navigationViewModel = (NavigationViewModel)viewModelResolver(typeof(NavigationViewModel));
 
-        // Act
         void act() => navigationService.NavigateTo<StubNotNavigatingViewModel, WindowContentNavigationMessage>(new WindowNavigationQuery());
 
-        //Accept
         Assert.Throws<NavigationDeniedException>(act);
     }
 
     [Fact]
     public void Should_does_not_throw_navigation_denied_exception_and_does_not_navigating()
     {
-        // Arrange
-        NavigationService navigationService = new(_viewModelResolver);
-        NavigationViewModel navigationViewModel = (NavigationViewModel)_viewModelResolver(typeof(NavigationViewModel));
+        Dictionary<Type, ViewModel> viewModels = CreateViewModelCollection();
+        Func<Type, ViewModel> viewModelResolver = (type) => viewModels[type];
+        NavigationService navigationService = new(viewModelResolver);
+        NavigationViewModel navigationViewModel = (NavigationViewModel)viewModelResolver(typeof(NavigationViewModel));
 
-        // Act
         navigationService.NavigateTo<StubNavigatingViewModel, StubNavigationMessage>(new StubNavigationQuery());
 
-        //Accept
         Assert.NotNull(navigationViewModel.CurrentViewModel);
         Assert.IsType<MainViewModel>(navigationViewModel.CurrentViewModel);
         Assert.IsNotType<StubNavigatingViewModel>(navigationViewModel.CurrentViewModel);
     }
 
-    private void InitialViewModels()
+    private static Dictionary<Type, ViewModel> CreateViewModelCollection()
     {
-        _viewModels.Add(typeof(MainViewModel), new MainViewModel());
-        _viewModels.Add(typeof(StubNavigatingViewModel), new StubNavigatingViewModel());
-        _viewModels.Add(typeof(StubNotNavigatingViewModel), new StubNotNavigatingViewModel());
-        _viewModels.Add(typeof(NavigationViewModel), new NavigationViewModel(new MainViewModel()));
+        var viewModels = new Dictionary<Type, ViewModel>()
+        {
+            { typeof(MainViewModel), new MainViewModel() },
+            { typeof(StubNavigatingViewModel), new StubNavigatingViewModel() },
+            { typeof(StubNotNavigatingViewModel), new StubNotNavigatingViewModel() },
+            { typeof(NavigationViewModel), new NavigationViewModel(new MainViewModel()) }
+        };
+
+        return viewModels;
     }
 }
