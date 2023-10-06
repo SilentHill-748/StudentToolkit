0c47f17d2f8be3e2bff5396abcd43af71db4ea9b
SilentHill <nikich213@gmail.com>
2023-10-06 16:09:45 +0500

    Change logic of openning dialog windows.

--- src/StudentToolkit/MVVM/ViewModels/Base/DialogViewModel.cs
+++ src/StudentToolkit/MVVM/ViewModels/Base/DialogViewModel.cs
@@ -1,11 +1,22 @@
-﻿using StudentToolkit.MVVM.Models.DialogResults;
+﻿using System;
+using System.Windows;
+
+using StudentToolkit.MVVM.Models.DialogResults;
 
 namespace StudentToolkit.MVVM.ViewModels.Base;
 
-public class DialogViewModel : ViewModel
+public class DialogViewModel : ValidatableViewModel
 {
     private DialogResult _result = new();
+    private ResizeMode _resizeMode;
 
+    public Action? Close { get; set; }
+
+    public ResizeMode ResizeMode
+    {
+        get => _resizeMode;
+        set => Set(ref _resizeMode, value);
+    }
     public DialogResult Result
     {
         get => _result;
