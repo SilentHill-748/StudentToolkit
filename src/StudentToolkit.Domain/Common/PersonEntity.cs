375c2fa28af8d6313ca9f67805dd9c5f15362dba
SilentHill <nikich213@gmail.com>
2023-10-09 19:03:08 +0500

    Add domain entities.

---
+++ src/StudentToolkit.Domain/Common/PersonEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentToolkit.Domain.Common;
+
+public abstract class PersonEntity : BaseEntity
+{
+    public string? FirstName { get; set; }
+    public string? MiddleName { get; set; }
+    public string? LastName { get; set; }
+    [NotMapped]
+    public string FullName => $"{MiddleName} {FirstName} {LastName}";
+    [NotMapped]
+    public string ShortFullName => $"{MiddleName} {FirstName![0]} {LastName![0]}";
+}
