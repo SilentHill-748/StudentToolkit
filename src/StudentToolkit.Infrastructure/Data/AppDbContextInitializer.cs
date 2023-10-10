aae4981aa6a29f89bb3f27d6aad0211a986ffa4d
SilentHill <nikich213@gmail.com>
2023-10-10 16:41:27 +0500

    Add logger on 'AppDbContextInitializer'.

--- src/StudentToolkit.Infrastructure/Data/AppDbContextInitializer.cs
+++ src/StudentToolkit.Infrastructure/Data/AppDbContextInitializer.cs
@@ -3,14 +3,27 @@
 public sealed class AppDbContextInitializer
 {
     private readonly AppDbContext _dbContext;
+    private readonly ILogger _logger;
 
-    public AppDbContextInitializer(AppDbContext appDbContext)
+    public AppDbContextInitializer(AppDbContext appDbContext, ILogger logger)
     {
+        ArgumentNullException.ThrowIfNull(appDbContext, nameof(appDbContext));
+        ArgumentNullException.ThrowIfNull(logger, nameof(logger));
+
         _dbContext = appDbContext;
+        _logger = logger;
     }
 
     public async Task InitializeAsync()
     {
-        await _dbContext.Database.MigrateAsync();
+        try
+        {
+            await _dbContext.Database.MigrateAsync();
+        }
+        catch (Exception ex)
+        {
+            _logger.Error(ex, "Database migration was stopped, because was be throw exception.");
+            throw;
+        }
     }
 }
--- src/StudentToolkit.Infrastructure/GlobalUsings.cs
+++ src/StudentToolkit.Infrastructure/GlobalUsings.cs
@@ -2,3 +2,4 @@
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 global using StudentToolkit.Domain.Entities;
+global using Serilog;
