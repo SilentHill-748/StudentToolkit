4bc2c454e3db5b85b70df3c4ab0ba25e5343d68f
SilentHill <nikich213@gmail.com>
2023-10-31 15:06:26 +0500

    Restruct of 'CreateGroupViewModel' to use 'GroupStore'.

--- src/StudentToolkit/MVVM/ViewModels/Presentation/CreateGroup/CreateGroupViewModel.cs
+++ src/StudentToolkit/MVVM/ViewModels/Presentation/CreateGroup/CreateGroupViewModel.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.ObjectModel;
-using System.Windows.Input;
+﻿using System.Windows.Input;
 
+using StudentToolkit.MVVM.Stores;
 using StudentToolkit.MVVM.Validation.CreateGroup;
 
 namespace StudentToolkit.MVVM.ViewModels.Presentation.CreateGroup;
@@ -9,36 +8,33 @@ namespace StudentToolkit.MVVM.ViewModels.Presentation.CreateGroup;
 public sealed class CreateGroupViewModel : ValidatableViewModel
 {
     private readonly CreateGroupViewModelValidator _validator;
-    private string _groupCode = string.Empty;
 
     public CreateGroupViewModel(
+        GroupStore groupStore,
+        ILogger logger,
         CreateGroupViewModelValidator validator,
         CreateStudentViewModelValidator studentValidator,
         NavigationService navigationService)
     {
+        ArgumentNullException.ThrowIfNull(groupStore, nameof(groupStore));
+        ArgumentNullException.ThrowIfNull(logger, nameof(logger));
         ArgumentNullException.ThrowIfNull(validator, nameof(validator));
         ArgumentNullException.ThrowIfNull(studentValidator, nameof(studentValidator));
         ArgumentNullException.ThrowIfNull(navigationService, nameof(navigationService));
-
+        
         _validator = validator;
+        Group = new GroupViewModel();
         WindowTitle = "Создание группы студентов";
 
-        Students = new ObservableCollection<StudentViewModel>();
-        Students.CollectionChanged += Students_CollectionChanged;
+        Group.Students.CollectionChanged += Students_CollectionChanged;
 
         ShowCreateStudentDialogCommand = new ShowCreateStudentDialogCommand(this, studentValidator);
-        CreateGroupCommand = new CreateGroupCommand(this, navigationService);
+        CreateGroupCommand = new CreateGroupCommand(logger, this, groupStore, navigationService);
 
         Validate(_validator, this);
     }
 
-    public ObservableCollection<StudentViewModel> Students { get; }
-
-    public string GroupCode
-    {
-        get => _groupCode;
-        set => ValidatableSet(_validator, this, ref _groupCode, value);
-    }
+    public GroupViewModel Group { get; }
 
     public ICommand ShowCreateStudentDialogCommand { get; }
     public ICommand CreateGroupCommand { get; }
--- src/StudentToolkit/WpfCore/Commands/CreateGroup/CreateGroupCommand.cs
+++ src/StudentToolkit/WpfCore/Commands/CreateGroup/CreateGroupCommand.cs
@@ -1,20 +1,31 @@
-﻿using StudentToolkit.WpfCore.Commands.Base;
+﻿using System.Threading.Tasks;
+
+using StudentToolkit.MVVM.Stores;
+using StudentToolkit.WpfCore.Commands.Base;
 
 namespace StudentToolkit.WpfCore.Commands.CreateGroup;
 
-public sealed class CreateGroupCommand : Command
+public sealed class CreateGroupCommand : AsyncCommand
 {
     private readonly CreateGroupViewModel _viewModel;
+    private readonly GroupStore _groupStore;
     private readonly NavigationService _navigationService;
 
-    public CreateGroupCommand(CreateGroupViewModel viewModel, NavigationService navigationService)
+    public CreateGroupCommand(
+        ILogger logger,
+        CreateGroupViewModel viewModel,
+        GroupStore groupStore,
+        NavigationService navigationService) : base(logger)
     {
         _viewModel = viewModel;
+        _groupStore = groupStore;
         _navigationService = navigationService;
     }
 
-    public override void Execute()
+    public override async Task ExecuteAsync()
     {
+        await _groupStore.CreateGroupAsync(_viewModel.Group);
+
         var navigationQuery = new WindowNavigationQuery();
 
         _navigationService.NavigateTo<MainViewModel, WindowContentNavigationMessage>(navigationQuery);
@@ -22,7 +33,8 @@ public sealed class CreateGroupCommand : Command
 
     public override bool CanExecute()
     {
-        return  _viewModel.HasNoErrors && 
-                _viewModel.Students.Count > 5;
+        return  !IsExecuting &&
+                _viewModel.HasNoErrors && 
+                _viewModel.Group.Students.Count > 5;
     }
 }
