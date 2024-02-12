6dbe38b486c48c6b09c36cd739796c5e3fbd46a6
SilentHill <nikich213@gmail.com>
2024-02-12 10:07:21 +0500

    Switch the default content view to 'GroupNotFoundView' for 'MainView'.

--- src/StudentToolkit/MVVM/ViewModels/MainViewModel.cs
+++ src/StudentToolkit/MVVM/ViewModels/MainViewModel.cs
@@ -1,12 +1,14 @@
-﻿namespace StudentToolkit.MVVM.ViewModels;
+﻿using StudentToolkit.MVVM.ViewModels.Presentation.GroupInfo;
+
+namespace StudentToolkit.MVVM.ViewModels;
 
 public class MainViewModel : ViewModel
 {
-    private ViewModel? _content;
+    private ViewModel? _contentViewModel;
 
     public MainViewModel(IGroupStore groupStore)
     {
-        _content = new AboutViewModel();
+        _contentViewModel = new GroupNotFoundViewModel();
 
         StatusBarViewModel = new StatusBarViewModel();
         WindowTitle = "Student Toolkit";
@@ -18,16 +20,16 @@ public class MainViewModel : ViewModel
 
         WeakReferenceMessenger.Default.Register<NavigationMessage>(this, (recipient, message) =>
         {
-            Content = message.GetDestinationViewModel<MainViewModel>() ?? Content;
+            ContentViewModel = message.GetDestinationViewModel<MainViewModel>() ?? ContentViewModel;
         });
     }
 
     public StatusBarViewModel StatusBarViewModel { get; }
 
-    public ViewModel? Content
+    public ViewModel? ContentViewModel
     {
-        get => _content;
-        set => Set(ref _content, value);
+        get => _contentViewModel;
+        set => Set(ref _contentViewModel, value);
     }
 
     public ICommand LoadedEventCommand { get; }
