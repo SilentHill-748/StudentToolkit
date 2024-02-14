ab5a1da0b4995cd6b57ed9cea7b831da39f88bd8
SilentHill <nikich213@gmail.com>
2024-02-14 12:27:03 +0500

    Add parameterless delegate command.

---
+++ src/StudentToolkit/WpfCore/Commands/Base/DelegateCommand.cs
@@ -0,0 +1,31 @@
+﻿namespace StudentToolkit.WpfCore.Commands.Base;
+
+public class DelegateCommand : Command
+{
+    private readonly Action _execute;
+    private readonly Func<bool>? _canExecute;
+
+    public DelegateCommand(Action execute)
+    {
+        ArgumentNullException.ThrowIfNull(execute, nameof(execute));
+
+        _execute = execute;
+    }
+
+    public DelegateCommand(Action execute, Func<bool> canExecute) : this(execute)
+    {
+        ArgumentNullException.ThrowIfNull(canExecute, nameof(canExecute));
+
+        _canExecute = canExecute;
+    }
+
+    public override void Execute()
+    {
+        _execute();
+    }
+
+    public override bool CanExecute()
+    {
+        return _canExecute is null || _canExecute();
+    }
+}
