feddbf33dcfe8a939591972c35f1b21b5210b22c
SilentHill <nikich213@gmail.com>
2024-02-22 13:11:19 +0500

    Replace calling of Close delegate to command.

--- src/StudentToolkit/MVVM/ViewModels/Notification/NotificationWithConfirmViewModel.cs
+++ src/StudentToolkit/MVVM/ViewModels/Notification/NotificationWithConfirmViewModel.cs
@@ -16,6 +16,6 @@ public class NotificationWithConfirmViewModel : NotificationViewModel
     {
         IsConfirmed = true;
 
-        Close?.Invoke();
+        CloseNotificationCommand.Execute(null);
     }
 }
