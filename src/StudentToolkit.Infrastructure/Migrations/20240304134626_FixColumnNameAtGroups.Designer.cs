e82eee967e1f58ec4df91694a9af8e51e1a28531
SilentHill <nikich213@gmail.com>
2024-03-04 18:48:13 +0500

    Fix column name at groups table.

--- src/StudentToolkit.Infrastructure/Migrations/20240304134626_FixColumnNameAtGroups.Designer.cs
+++ src/StudentToolkit.Infrastructure/Migrations/20240304134626_FixColumnNameAtGroups.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StudentToolkit.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using StudentToolkit.Infrastructure.Data;
 namespace StudentToolkit.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240304134626_FixColumnNameAtGroups")]
+    partial class FixColumnNameAtGroups
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
