919edf21fd409b3d1404c686a13ba8bb50b36732
SilentHill <nikich213@gmail.com>
2024-03-07 10:39:26 +0500

    Add StudentViewModel.

---
+++ src/StudentToolkit/MVVM/ViewModels/Model/StudentViewModel.cs
@@ -0,0 +1,64 @@
+﻿namespace StudentToolkit.MVVM.ViewModels.Model;
+
+public class StudentViewModel : ValidatableViewModel
+{
+    private string _firstName = string.Empty;
+    private string _lastName = string.Empty;
+    private string _middleName = string.Empty;
+
+    public Guid Id { get; set; }
+    public string FullName => GetFullName();
+
+    public string FirstName
+    {
+        get => _firstName;
+        set => SetWithValidation(ref _firstName, value.Trim());
+    }
+    public string LastName
+    {
+        get => _lastName;
+        set => SetWithValidation(ref _lastName, value.Trim());
+    }
+    public string MiddleName
+    {
+        get => _middleName;
+        set => SetWithValidation(ref _middleName, value.Trim());
+    }
+
+    public void ClearNameProperties(bool isCleanValidationErrors = false)
+    {
+        FirstName = string.Empty;
+        LastName = string.Empty;
+        MiddleName = string.Empty;
+
+        if (isCleanValidationErrors)
+            ClearErrors();
+    }
+
+    public override string ToString()
+    {
+        return GetShortFullName();
+    }
+
+    private string GetFullName()
+    {
+        return CheckNameProperties()
+            ? ""
+            : $"{LastName} {FirstName} {MiddleName}";
+    }
+
+    private string GetShortFullName()
+    {
+        return CheckNameProperties()
+            ? ""
+            : $"{LastName} {FirstName[0]}. {MiddleName[0]}.";
+    }
+
+    private bool CheckNameProperties()
+    {
+        return
+           string.IsNullOrWhiteSpace(FirstName) &&
+           string.IsNullOrWhiteSpace(LastName) &&
+           string.IsNullOrWhiteSpace(MiddleName);
+    }
+}
