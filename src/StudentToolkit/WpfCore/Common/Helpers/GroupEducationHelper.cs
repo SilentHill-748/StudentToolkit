09eca0b034d88d820dfa2a2ce93cc2516ee2e0f8
SilentHill <nikich213@gmail.com>
2024-03-08 12:11:49 +0500

    Add helper to get selective education values.

---
+++ src/StudentToolkit/WpfCore/Common/Helpers/GroupEducationHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace StudentToolkit.WpfCore.Common.Helpers;
+
+public static class GroupEducationHelper
+{
+    public static ObservableCollection<string> CreateEducationFormats()
+    {
+        return
+        [
+            "Очно",
+            "Очно-заочно",
+            "Заочно"
+        ];
+    }
+
+    public static ObservableCollection<string> CreateEducationTypes()
+    {
+        return 
+        [
+            "СПО",
+            "Бакалавриат",
+            "Магистратура",
+            "Аспирантура"
+        ];
+    }
+}
