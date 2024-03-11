e24eeb7078cb9c7187f248092b8844a24e607a77
SilentHill <nikich213@gmail.com>
2024-03-11 12:34:48 +0500

    Add GroupInfoViewModel.

---
+++ src/StudentToolkit/MVVM/ViewModels/Presentation/Group/Info/GroupInfoViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentToolkit.MVVM.ViewModels.Presentation.Group.Info;
+
+public class GroupInfoViewModel : ViewModel
+{
+    public GroupInfoViewModel(GroupViewModel group)
+    {
+        ArgumentNullException.ThrowIfNull(group, nameof(group));
+
+        Group = group;
+    }
+
+    public GroupViewModel Group { get; }
+}
