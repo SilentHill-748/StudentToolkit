f4af64b4eb12534493f5255012cc523d9c7922d5
SilentHill <nikich213@gmail.com>
2024-03-11 09:47:46 +0500

    Move views for creating group to special folder.

---
+++ src/StudentToolkit/MVVM/Views/Group/Create/AddStudentsToGroupView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace StudentToolkit.MVVM.Views.Group.Create;
+
+public partial class AddStudentsToGroupView : UserControl
+{
+    public AddStudentsToGroupView()
+    {
+        InitializeComponent();
+    }
+}
--- src/StudentToolkit/MVVM/Views/Group/Create/InputGroupDataView.xaml.cs
+++ src/StudentToolkit/MVVM/Views/Group/Create/InputGroupDataView.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 
-namespace StudentToolkit.MVVM.Views.Group;
+namespace StudentToolkit.MVVM.Views.Group.Create;
 
 public partial class InputGroupDataView : UserControl
 {
