484b4995c70427efec4de2d7a7ccbc84095dc887
SilentHill <nikich213@gmail.com>
2024-03-12 15:57:54 +0500

    Add TileContentControl.

---
+++ src/StudentToolkit/WpfCore/CustomControls/TileContentControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace StudentToolkit.WpfCore.CustomControls;
+
+[ContentProperty("Content")]
+public class TileContentControl : Control
+{
+    public static readonly DependencyProperty ContentProperty
+        = DependencyProperty.Register(nameof(Content), typeof(object), typeof(TileContentControl), new PropertyMetadata(null));
+
+    public object Content
+    {
+        get => GetValue(ContentProperty);
+        set => SetValue(ContentProperty, value);
+    }
+}
