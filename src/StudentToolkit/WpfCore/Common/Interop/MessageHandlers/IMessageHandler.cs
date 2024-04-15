a400cd2d3e0364acf80565729db57f67cac1e8a6
SilentHill <nikich213@gmail.com>
2024-04-15 20:43:30 +0500

    Add 'IMessageHandler'.

---
+++ src/StudentToolkit/WpfCore/Common/Interop/MessageHandlers/IMessageHandler.cs
@@ -0,0 +1,10 @@
+﻿using StudentToolkit.WpfCore.Common.Interop.Structs;
+
+namespace StudentToolkit.WpfCore.Common.Interop.MessageHandlers;
+
+internal interface IMessageHandler
+{
+    int Message { get; }
+
+    bool Handle(IntMessageArgs args);
+}
