7a740a3008eef44d1ddc6f019b86c2ec8d34cac1
SilentHill <nikich213@gmail.com>
2024-04-18 15:57:12 +0500

    Add 'InputField' custom control.

---
+++ src/StudentToolkit/WpfCore/CustomControls/InputField.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace StudentToolkit.WpfCore.CustomControls;
+
+[ContentProperty(nameof(Field))]
+public class InputField : Control
+{
+    public static readonly DependencyProperty TitleProperty =
+        DependencyProperty.Register(
+            nameof(Title),
+            typeof(object),
+            typeof(InputField),
+            new PropertyMetadata(null));
+
+    public static readonly DependencyProperty FieldProperty =
+        DependencyProperty.Register(
+            nameof(Field),
+            typeof(object),
+            typeof(InputField),
+            new PropertyMetadata(null));
+
+    public object Title
+    {
+        get => GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
+
+    public object Field
+    {
+        get => GetValue(FieldProperty);
+        set => SetValue(FieldProperty, value);
+    }
+}
