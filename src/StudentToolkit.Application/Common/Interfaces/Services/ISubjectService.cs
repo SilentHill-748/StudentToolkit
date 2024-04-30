89acf6ccca6fbd005d08a79df3de9e92468577bd
SilentHill <nikich213@gmail.com>
2024-04-30 13:17:37 +0500

    Add 'SubjectService'.

---
+++ src/StudentToolkit.Application/Common/Interfaces/Services/ISubjectService.cs
@@ -0,0 +1,16 @@
+﻿namespace StudentToolkit.Application.Common.Interfaces.Services;
+
+public interface ISubjectService
+{
+    Task AddSubjectAsync(SubjectDto subjectDto);
+    Task AddSubjectsAsync(IEnumerable<SubjectDto> subjectDtos);
+
+    Task DeleteSubjectAsync(SubjectDto subjectDto);
+    Task DeleteAllSubjectsAsync();
+
+    Task UpdateSubjectAsync(SubjectDto subjectDto);
+    Task UpdateSubjects(IEnumerable<SubjectDto> subjectDtos);
+
+    Task<SubjectDto> GetSubjectByIdAsync(Guid id);
+    IEnumerable<SubjectDto> GetAllSubjects();
+}
---
+++ src/StudentToolkit.Application/Services/SubjectService.cs
@@ -0,0 +1,98 @@
+﻿
+namespace StudentToolkit.Application.Services;
+
+public class SubjectService : Service, ISubjectService
+{
+    public SubjectService(IAppDbContext appDbContext)
+        : base(appDbContext)
+    { }
+
+    public async Task AddSubjectAsync(SubjectDto subjectDto)
+    {
+        Subject subjectEntity = subjectDto.Adapt<Subject>();
+
+        await DbContext.Subjects.AddAsync(subjectEntity);
+        await DbContext.SaveChangesAsync();
+    }
+
+    public async Task AddSubjectsAsync(IEnumerable<SubjectDto> subjectDtos)
+    {
+        IEnumerable<Subject> subjectEntities =
+            subjectDtos.Adapt<IEnumerable<Subject>>();
+
+        await DbContext.Subjects.AddRangeAsync(subjectEntities);
+        await DbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteSubjectAsync(SubjectDto subjectDto)
+    {
+        Subject subjectEntity =
+            await InternalGetSubjectByIdAsync(subjectDto.Id);
+
+        DbContext.Subjects.Remove(subjectEntity);
+
+        await DbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteAllSubjectsAsync()
+    {
+        IQueryable<Subject> subjectEntities = DbContext.Subjects;
+
+        DbContext.Subjects.RemoveRange(subjectEntities);
+        
+        await DbContext.SaveChangesAsync();
+    }
+
+    public async Task<SubjectDto> GetSubjectByIdAsync(Guid id)
+    {
+        Subject subjectEntity =
+            await InternalGetSubjectByIdAsync(id, QueryTrackingBehavior.NoTracking);
+
+        return subjectEntity.Adapt<SubjectDto>();
+    }
+
+    public IEnumerable<SubjectDto> GetAllSubjects()
+    {
+        return DbContext
+            .Subjects
+            .AsNoTracking()
+            .ProjectToType<SubjectDto>();
+    }
+
+    public async Task UpdateSubjectAsync(SubjectDto subjectDto)
+    {
+        await InternalUpdateSubject(subjectDto);
+        
+        await DbContext.SaveChangesAsync();
+    }
+
+    public async Task UpdateSubjects(IEnumerable<SubjectDto> subjectDtos)
+    {
+        foreach (SubjectDto subjectDto in subjectDtos)
+        {
+            await InternalUpdateSubject(subjectDto);
+        }
+
+        await DbContext.SaveChangesAsync();
+    }
+
+    private async Task InternalUpdateSubject(SubjectDto subjectDto)
+    {
+        Subject subjectEntity =
+            await InternalGetSubjectByIdAsync(subjectDto.Id);
+
+        subjectEntity.Name = subjectDto.Name;
+
+        DbContext.Subjects.Update(subjectEntity);
+    }
+
+    private async Task<Subject> InternalGetSubjectByIdAsync(
+        Guid id,
+        QueryTrackingBehavior trackingBehavior = QueryTrackingBehavior.TrackAll)
+    {
+        return await DbContext.Subjects
+            .AsTrackingWithStrategy(trackingBehavior)
+            .FirstOrDefaultAsync(subject => subject.Id == id)
+                ?? throw new Exception();
+    }
+}
