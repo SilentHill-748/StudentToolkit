f3cc1faae76be27b84f2ff0adc4423b6bde74a72
SilentHill <nikich213@gmail.com>
2024-09-10 10:20:37 +0500

    Add a new UI element 'InputField'.

---
+++ src/StudentToolkit.Presentation/Controls/InputField.cs
@@ -0,0 +1,28 @@
+﻿namespace StudentToolkit.Presentation.Controls;
+
+public class InputField : Control
+{
+    public static readonly DependencyProperty TitleProperty =
+        DependencyProperty.Register(
+            nameof(Title),
+            typeof(object),
+            typeof(InputField));
+
+    public static readonly DependencyProperty FieldProperty =
+        DependencyProperty.Register(
+            nameof(Field),
+            typeof(object),
+            typeof(InputField));
+
+    public object Title
+    {
+        get => GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
+
+    public object Field
+    {
+        get => GetValue(FieldProperty);
+        set => SetValue(FieldProperty, value);
+    }
+}
