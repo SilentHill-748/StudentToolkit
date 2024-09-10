526ebb0e1e64350344ffa06c3e3db7e49a00c90c
SilentHill <nikich213@gmail.com>
2024-09-10 10:11:56 +0500

    Add a new UI element 'Tile'.

---
+++ src/StudentToolkit.Presentation/Controls/Tile.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Markup;
+
+namespace StudentToolkit.Presentation.Controls;
+
+[ContentProperty("Content")]
+public class Tile : Control
+{
+    public static readonly DependencyProperty ContentProperty =
+        DependencyProperty.Register(
+            nameof(Content),
+            typeof(object),
+            typeof(Tile));
+
+    public object Content
+    {
+        get => GetValue(ContentProperty);
+        set => SetValue(ContentProperty, value);
+    }
+}
